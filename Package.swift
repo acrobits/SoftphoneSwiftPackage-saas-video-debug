66c2c910ec98c84ea6d43339db5bbdd54b28c35f
Autobuild <autobuild@rafmini.inner.acrobits.cz>
2025-12-29 15:00:20 +0100

    Package updated to 25.3.3

--- Package.swift
+++ Package.swift
@@ -14,11 +14,11 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Softphone",
-            url: "https://dist.acrobits.net/saas-video-swift-package/debug/Softphone.xcframework-25.3.2.zip",
-            checksum: "5c40e9d49c847d6061b42c51ad66aca13539a909dd759e5d7f3cbdecc53ef3d3"),
+            url: "https://dist.acrobits.net/saas-video-swift-package/debug/Softphone.xcframework-25.3.3.zip",
+            checksum: "bded62d79e7d598e8850d9d318c23e1e4bd439dff1e39a6bbc800e543e9cc735"),
         .binaryTarget(
             name: "Softphone_Swift",
-            url: "https://dist.acrobits.net/saas-video-swift-package/debug/Softphone_Swift.xcframework-25.3.2.zip",
-            checksum: "778fe167f59428386f68464ff7ebd90ac0085dfbf2e4c07b05b5207f52e79b6e"),
+            url: "https://dist.acrobits.net/saas-video-swift-package/debug/Softphone_Swift.xcframework-25.3.3.zip",
+            checksum: "57c0513bbdf7ea9f7a40c1493fb3eca2d2a65890b15e280c1b7e52bd990c2f97"),
         ]
 )
